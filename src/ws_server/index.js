6337885a372493ee5894676ee7bcb80065f14297
Donchidzee <donchik020202@gmail.com>
2024-10-28 18:04:31 +0100

    feat: add message handler

--- src/ws_server/index.js
+++ src/ws_server/index.js
@@ -1,5 +1,14 @@
 import { WebSocketServer } from "ws";
+import { handleMessage } from "./messageHandler.js";
 
 const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    const parsedMessage = JSON.parse(message);
+    console.log(`Received message: ${message}`);
+    handleMessage(ws, parsedMessage);
+  });
+});
 
 export { wss };
