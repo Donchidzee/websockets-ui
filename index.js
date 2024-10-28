8eaa420249e1125573f0355b5af3a441e38fe71b
Donchidzee <donchik020202@gmail.com>
2024-10-28 18:02:32 +0100

    feat: add ws_server and data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { httpServer } from "./src/http_server/index.js";
+import { wss } from "./src/ws_server/index.js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+console.log(`WebSocket server is running on port 3000`);
